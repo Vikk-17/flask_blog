cd5f9a330513f753d54ab422f5ede7579536c2e3
Souvik <souvikc3030@gmail.com>
2022-08-05 23:40:21 +0530

    added jinja2 template

--- server.py
+++ server.py
@@ -3,10 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/<string:page_name>')
-def html_page(page_name):
-    return render_template(page_name)
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
 
