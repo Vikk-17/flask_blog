0bce025582d05c44d88772abb83adf8fdbdf48b0
Souvik <souvikc3030@gmail.com>
2022-08-06 13:55:44 +0530

    connected-database-mysql

--- server.py
+++ server.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/blog_base" # mysql://username:password@server/db
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    '''
+    s_no, name, email, phone_number, message, date
+    '''
+    s_no = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_number = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=False)
 
 @app.route('/')
 def home():
@@ -14,9 +30,26 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == "POST":
+        data = request.form.to_dict()
+        name = data['name']
+        email = data['e_address']
+        number = data['number']
+        message = data['message']
+        '''
+        s_no, name, email, phone_number, message, date
+        '''
+        # print(name, email, number, message)
+        entry = Contacts(name=name, email=email, phone_number=number, message=message, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+
+    return render_template('/contact.html')
+
+
+
 
 @app.route('/post')
 def post():
